40347c4bc894d89ec697c672f699958ffd1390bc
Johan Bregell <johan.bregell@fingerprints.com>
2019-03-13 00:36:40 +0100

    Added working volume handling

--- Dockerfile
+++ Dockerfile
@@ -3,31 +3,15 @@ MAINTAINER Johan Bregell
 
 ENV DISPLAY=:0
 
-ENV CONFIG /home/steam/.wine/drive_c/users/steam/Application\ Data/SpaceEngineersDedicated
-ENV MODS $CONFIG/Mods
-ENV SAVES $CONFIG/Saves
-ENV UPDATER $CONFIG/Updater
-
 USER root
 RUN mkdir -p /mnt/steam/space-engineers-server
-WORKDIR /mnt/steam/space-engineers-server
-RUN mkdir -p /mnt/steam/space-engineers-server/config
-COPY Nerdz/ Nerdz
+WORKDIR /home/steam
 COPY entrypoint.sh /entrypoint.sh
-COPY SpaceEngineers-Dedicated.cfg SpaceEngineers-Dedicated.cfg
 RUN chmod +x /entrypoint.sh
-
-RUN ln -s $CONFIG /mnt/steam/space-engineers-server/config
-#RUN mkdir -p /home/steam/.wine/drive_c/users/steam/Application\ Data/SpaceEngineersDedicated
-#RUN mkdir -p $MODS
-#RUN mkdir -p $SAVES
-#RUN mkdir -p $UPDATER
-#RUN cp /mnt/steam/space-engineers-server/SpaceEngineers-Dedicated.cfg /home/steam/.wine/drive_c/users/steam/Application\ Data/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
-
-RUN chown -R steam /mnt/steam/space-engineers-server /home/steam/*
-
-VOLUME /mnt/steam/space-engineers-server
-VOLUME /home/steam/.wine/drive_c/users/steam/Application\ Data/SpaceEngineersDedicated
+RUN chown -R steam /home/steam/*
+RUN ls -l
 
 USER steam
 ENTRYPOINT ["/entrypoint.sh"]
+
+VOLUME /mnt/steam/space-engineers-server
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,22 +2,11 @@
 
 set -e
 
-mkdir -p /home/steam/.wine/drive_c/users/steam/Application\ Data/SpaceEngineersDedicated
-mkdir -p /home/steam/.wine/drive_c/users/steam/Application\ Data/SpaceEngineersDedicated/Saves
-mkdir -p /home/steam/.wine/drive_c/users/steam/Application\ Data/SpaceEngineersDedicated/Mods
-mkdir -p /home/steam/.wine/drive_c/users/steam/Application\ Data/SpaceEngineersDedicated/Updater
-
-if [ ! -f /home/steam/.wine/drive_c/users/steam/Application\ Data/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg ]; then
-	# Copy default settings if SpaceEngineers-Dedicated.cfg doesn't exist
-	cp /mnt/steam/space-engineers-server/SpaceEngineers-Dedicated.cfg /home/steam/.wine/drive_c/users/steam/Application\ Data/SpaceEngineersDedicated/SpaceEngineers-Dedicated.cfg
-	ls -l /home/steam/.wine/drive_c/users/steam/Application\ Data/SpaceEngineersDedicated/
-fi
-
-if [ ! -d /home/steam/.wine/drive_c/users/steam/Application\ Data/SpaceEngineersDedicated/Saves/Nerdz/ ]; then
-	# Copy default save if Nerdz doesn't exist
-	cp -r /mnt/steam/space-engineers-server/Nerdz /home/steam/.wine/drive_c/users/steam/Application\ Data/SpaceEngineersDedicated/Saves/Nerdz
-	ls -l /home/steam/.wine/drive_c/users/steam/Application\ Data/SpaceEngineersDedicated/Saves/
-fi
+mkdir -p /mnt/steam/space-engineers-server/config
+ln -s /mnt/steam/space-engineers-server/config /home/steam/.wine/drive_c/users/steam/Application\ Data/SpaceEngineersDedicated
+mkdir -p /mnt/steam/space-engineers-server/config/Saves
+mkdir -p /mnt/steam/space-engineers-server/config/Mods
+mkdir -p /mnt/steam/space-engineers-server/config/Updater
 
 /home/steam/steamcmd/steamcmd.sh +login anonymous +force_install_dir /mnt/steam/space-engineers-server/ +app_update 298740 +quit
 wine64 /mnt/steam/space-engineers-server/DedicatedServer64/SpaceEngineersDedicated.exe -console
