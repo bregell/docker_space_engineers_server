a39a6637764d0db610a311b31b365af9d6eebedc
Johan Bregell <johan.bregell@fingerprints.com>
2019-03-13 14:22:36 +0100

    Added application data as volume
    Also added SERVER_NAME variable.

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,20 @@ FROM bregell/wine
 MAINTAINER Johan Bregell
 
 ENV DISPLAY=:0
+ENV CONFIG "/home/steam/.wine/drive_c/users/steam/Application Data/SpaceEngineersDedicated"
+ENV SERVER_NAME DefaultSavePath
 
 USER root
 RUN mkdir -p /mnt/steam/space-engineers-server
+RUN mkdir -p $CONFIG
 WORKDIR /home/steam
 COPY entrypoint.sh /entrypoint.sh
+COPY SpaceEngineers-Dedicated.cfg /home/steam/SpaceEngineers-Dedicated.cfg
 RUN chmod +x /entrypoint.sh
 RUN chown -R steam /home/steam/*
-RUN ls -l
 
 USER steam
 ENTRYPOINT ["/entrypoint.sh"]
 
 VOLUME /mnt/steam/space-engineers-server
+VOLUME $CONFIG
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,40 @@
 #!/bin/bash -x
 
-set -e
+if [ ! -d "$CONFIG"]; then
+	# Setup folder for application data
+	mkdir -p "$CONFIG"
+fi
 
-mkdir -p /mnt/steam/space-engineers-server/config
-ln -s /mnt/steam/space-engineers-server/config /home/steam/.wine/drive_c/users/steam/Application\ Data/SpaceEngineersDedicated
-mkdir -p /mnt/steam/space-engineers-server/config/Saves
-mkdir -p /mnt/steam/space-engineers-server/config/Mods
-mkdir -p /mnt/steam/space-engineers-server/config/Updater
+if [ ! -d "$CONFIG"/Saves ]; then
+	# Setup folder for saves
+	mkdir -p "$CONFIG"/Saves
+fi
+
+if [ ! -d "$CONFIG"/Mods ]; then
+	# Setup folder for mods
+	mkdir -p "$CONFIG"/Mods
+fi
+
+if [ ! -d "$CONFIG"/Updater ]; then
+	# Setup folder for updater
+	mkdir -p "$CONFIG"/Updater
+fi
+
+if [ ! -d "$CONFIG"/Saves/"$SERVER_NAME" ]; then
+	# Setup folder for default save
+	mkdir -p "$CONFIG"/Saves/"$SERVER_NAME"
+fi
+
+if [ ! -f "$CONFIG"/SpaceEngineers-Dedicated.cfg ]; then
+	# Copy standard config file to correct location
+	cp /home/steam/SpaceEngineers-Dedicated.cfg "$CONFIG"
+	sed -i 's/DefaultSavePath/'$SERVER_NAME'/g' "$CONFIG"/SpaceEngineers-Dedicated.cfg
+fi
+
+if [ ! -d /mnt/steam/space-engineers-server ]; then
+	# Setup filesystem for application data
+	mkdir -p /mnt/steam/space-engineers-server
+fi
 
 /home/steam/steamcmd/steamcmd.sh +login anonymous +force_install_dir /mnt/steam/space-engineers-server/ +app_update 298740 +quit
 wine64 /mnt/steam/space-engineers-server/DedicatedServer64/SpaceEngineersDedicated.exe -console
