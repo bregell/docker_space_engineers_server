9a1728e2c940f0aadf785bb0c68ad05aa573f798
Johan Bregell <johan.bregell@fingerprints.com>
2019-03-20 00:27:16 +0100

    Change user to root
    Use -path when starting server to ease path creation.
    Add simple docker-compose.yml
    Update Standard config file.

--- Dockerfile
+++ Dockerfile
@@ -2,20 +2,20 @@ FROM bregell/wine
 MAINTAINER Johan Bregell
 
 ENV DISPLAY=:0
-ENV CONFIG "/home/steam/.wine/drive_c/users/steam/Application Data/SpaceEngineersDedicated"
+ENV WORK "/mnt/root/space-engineers-server"
+ENV CONFIG "/mnt/root/space-engineers-server/config"
 ENV SERVER_NAME DefaultSavePath
 
 USER root
-RUN mkdir -p /mnt/steam/space-engineers-server
-RUN mkdir -p $CONFIG
-WORKDIR /home/steam
+RUN mkdir -p ${WORK}
+RUN mkdir -p ${CONFIG}
+WORKDIR /home/root
 COPY entrypoint.sh /entrypoint.sh
-COPY SpaceEngineers-Dedicated.cfg /home/steam/SpaceEngineers-Dedicated.cfg
+COPY SpaceEngineers-Dedicated.cfg /home/root/SpaceEngineers-Dedicated.cfg
 RUN chmod +x /entrypoint.sh
-RUN chown -R steam /home/steam/*
 
-USER steam
+WORKDIR ${WORK}
 ENTRYPOINT ["/entrypoint.sh"]
 
-VOLUME /mnt/steam/space-engineers-server
-VOLUME $CONFIG
+VOLUME ${WORK}
+#VOLUME ${CONFIG}
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,40 +1,45 @@
-#!/bin/bash -x
+#!/bin/bash
 
-if [ ! -d "$CONFIG"]; then
+set -x
+
+if [ ! -d ${CONFIG} ]; then
 	# Setup folder for application data
-	mkdir -p "$CONFIG"
+	mkdir -p ${CONFIG}
 fi
 
-if [ ! -d "$CONFIG"/Saves ]; then
+if [ ! -d ${CONFIG}/Saves ]; then
 	# Setup folder for saves
-	mkdir -p "$CONFIG"/Saves
+	mkdir -p ${CONFIG}/Saves
 fi
 
-if [ ! -d "$CONFIG"/Mods ]; then
+if [ ! -d ${CONFIG}/Mods ]; then
 	# Setup folder for mods
-	mkdir -p "$CONFIG"/Mods
+	mkdir -p ${CONFIG}/Mods
 fi
 
-if [ ! -d "$CONFIG"/Updater ]; then
+if [ ! -d ${CONFIG}/Updater ]; then
 	# Setup folder for updater
-	mkdir -p "$CONFIG"/Updater
+	mkdir -p ${CONFIG}/Updater
 fi
 
-if [ ! -d "$CONFIG"/Saves/"$SERVER_NAME" ]; then
+if [ ! -d ${CONFIG}/Saves/"$SERVER_NAME" ]; then
 	# Setup folder for default save
-	mkdir -p "$CONFIG"/Saves/"$SERVER_NAME"
+	mkdir -p ${CONFIG}/Saves/"$SERVER_NAME"
 fi
 
-if [ ! -f "$CONFIG"/SpaceEngineers-Dedicated.cfg ]; then
+ls -la ${CONFIG}
+
+if [ ! -f ${CONFIG}/SpaceEngineers-Dedicated.cfg ]; then
 	# Copy standard config file to correct location
-	cp /home/steam/SpaceEngineers-Dedicated.cfg "$CONFIG"
-	sed -i 's/DefaultSavePath/'$SERVER_NAME'/g' "$CONFIG"/SpaceEngineers-Dedicated.cfg
+	cp /home/root/SpaceEngineers-Dedicated.cfg ${CONFIG}
+	sed -i 's/DefaultSavePath/'$SERVER_NAME'/g' ${CONFIG}/SpaceEngineers-Dedicated.cfg
 fi
 
-if [ ! -d /mnt/steam/space-engineers-server ]; then
+if [ ! -d ${WORK} ]; then
 	# Setup filesystem for application data
-	mkdir -p /mnt/steam/space-engineers-server
+	mkdir -p ${WORK}
 fi
 
-/home/steam/steamcmd/steamcmd.sh +login anonymous +force_install_dir /mnt/steam/space-engineers-server/ +app_update 298740 +quit
-wine64 /mnt/steam/space-engineers-server/DedicatedServer64/SpaceEngineersDedicated.exe -console
+/home/root/steamcmd/steamcmd.sh +login anonymous +force_install_dir ${WORK} +app_update 298740 +quit
+cd ${WORK}/DedicatedServer64
+wine64 SpaceEngineersDedicated.exe -console -ignorelastsession -path Z:\\mnt\\root\\space-engineers-server\\config
