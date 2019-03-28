524a50ae9fc23b4a9942c98c9a0ef3e42addc7bd
Johan Bregell <johan.bregell@fingerprints.com>
2019-03-28 21:05:35 +0100

    Fixed some bugs

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,13 @@
 
 set -x
 
+if [ ! -d ${WORK} ]; then
+	# Setup folder for steamcmd data
+	mkdir -p ${WORK}
+fi
+
 if [ ! -d ${CONFIG} ]; then
-	# Setup folder for application data
+	# Setup folder for space engineers data
 	mkdir -p ${CONFIG}
 fi
 
@@ -22,24 +27,20 @@ if [ ! -d ${CONFIG}/Updater ]; then
 	mkdir -p ${CONFIG}/Updater
 fi
 
-if [ ${WORK}/${SERVER_NAME}]; then
+if [ -d ${WORK}/${SERVER_NAME} ]; then
 	# Copy save to save location
 	cp -r ${WORK}/${SERVER_NAME} ${CONFIG}/Saves/
+	chown -R root:root ${CONFIG}/Saves/${SERVER_NAME}
 fi
-
-ls -la ${CONFIG}
 
 if [ ! -f ${CONFIG}/SpaceEngineers-Dedicated.cfg ]; then
 	# Copy standard config file to correct location
 	cp /home/root/SpaceEngineers-Dedicated.cfg ${CONFIG}
-	sed -i 's/DefaultSavePath/'$SERVER_NAME'/g' ${CONFIG}/SpaceEngineers-Dedicated.cfg
 fi
 
-if [ ! -d ${WORK} ]; then
-	# Setup filesystem for application data
-	mkdir -p ${WORK}
-fi
+# Change save path to value from config
+sed -i 's/DefaultSavePath/'${SERVER_NAME}'/g' ${CONFIG}/SpaceEngineers-Dedicated.cfg
 
 /home/root/steamcmd/steamcmd.sh +login anonymous +force_install_dir ${WORK} +app_update 298740 +quit
 cd ${WORK}/DedicatedServer64
-wine64 SpaceEngineersDedicated.exe -noconsole -ignorelastsession -path Z:\\mnt\\root\\space-engineers-server\\config
+wine SpaceEngineersDedicated.exe -noconsole -ignorelastsession -path Z:\\mnt\\root\\space-engineers-server\\config
